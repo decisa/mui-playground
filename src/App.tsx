bc1c6c6e8460e0ad7d60f8c9a39af93a8e205ccc
Artem Telesh <decarea@yahoo.com>
2023-02-24 00:16:32 -0500

    starter react18 typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const x = (a: number, b: number) => a * 2 + b
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Hello World {x(3, 5)}</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
