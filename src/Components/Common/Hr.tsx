b2877d343fab87dfaa8fe10a9ed382f4e97c3fd0
Art Telesh <decarea@yahoo.com>
2024-03-23 12:51:27 -0400

    added more style options

--- src/Components/Common/Hr.tsx
+++ src/Components/Common/Hr.tsx
@@ -1,17 +1,24 @@
 import { useTheme } from '@mui/material'
 import { tokens } from '../../theme'
 
-export default function Hr() {
+type HRProps = {
+  my?: number
+  mx?: number
+}
+export default function Hr({ my, mx }: HRProps) {
   const theme = useTheme()
   const colors = tokens(theme.palette.mode)
 
   return (
     <hr
       style={{
-        marginTop: 0,
         height: '2px',
         backgroundColor: colors.blueAccent[200],
         border: 'none',
+        marginTop: my !== undefined ? `${my * 8}px` : 0,
+        marginBottom: my !== undefined ? `${my * 8}px` : undefined,
+        marginLeft: mx !== undefined ? `${mx * 8}px` : undefined,
+        marginRight: mx !== undefined ? `${mx * 8}px` : undefined,
       }}
     />
   )
