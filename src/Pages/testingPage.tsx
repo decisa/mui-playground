02c501c2402d4537bc10d51bd749a5340f106c59
Art Telesh <decarea@yahoo.com>
2024-03-27 15:57:57 -0400

    testing page with create new delivery form

--- src/Pages/testingPage.tsx
+++ src/Pages/testingPage.tsx
@@ -2,21 +2,23 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Box } from '@mui/material'
 import { Result, ResultAsync } from 'neverthrow'
 import { useLoaderData } from 'react-router'
+import { create } from 'domain'
 import { Address, DeliveryMethod, FullOrder } from '../Types/dbtypes'
 import {
+  createDelivery,
   getDeliveryMethods,
   getOrderAddresses,
   getOrderByNumber,
 } from '../utils/inventoryManagement'
 import { useSnackBar } from '../Components/GlobalSnackBar'
-import DeliveryForm, { DeliveryFormValues } from '../Forms/DeliveryForm'
+import DeliveryForm, { prepareDeliveryFormData } from '../Forms/DeliveryForm'
 import { getDeliveryName, isCoiRequired } from '../utils/scheduleUtils'
 
 let orderNumber = '100005081'
 orderNumber = '100008122'
 orderNumber = '100008039'
 orderNumber = '100007450' // mila kushnir
-orderNumber = '100008298'
+orderNumber = '100008184'
 
 export default function TestingPage() {
   const snack = useSnackBar()
@@ -66,77 +68,54 @@ export default function TestingPage() {
       })
   }, [order?.id, snack])
 
-  const initValues: DeliveryFormValues = useMemo(() => {
-    let shippingAddressId = order?.shippingAddress?.id || 0
-    if (orderAddresses?.length > 0) {
-      if (!orderAddresses.find((address) => address.id === shippingAddressId)) {
-        shippingAddressId = orderAddresses[0]?.id || 0
-      }
-    }
-
-    const items =
-      order?.products.map((product) => {
-        const { id, configuration } = product
-        const {
-          // qtyOrdered,
-          summary,
-        } = configuration
-        const {
-          qtyConfirmed = 0, // delivery confirmed by customer
-          qtyReceived = 0, // at the warehouse
-          // qtyPlanned = 0, // delivery created
-          // qtyPurchased = 0, // from vendor
-          qtyScheduled = 0, // delivery added to a schedule
-          // qtyShipped = 0, // from vendor
-        } = summary
-        const ready = qtyReceived - qtyConfirmed - qtyScheduled
-        return {
-          configurationId: id,
-          qtyToShip: ready,
-        }
-      }) || []
-
-    return {
-      orderId: order?.id || 0,
-      coiRequired: isCoiRequired(order),
-      coiReceived: false,
-      coiNotes: 'waiting for COI sample form from customer',
-      notes: 'please call 30 min in advance',
-      title: getDeliveryName(order),
-      days: [false, true, true, true, true, true, false],
-      estimatedDuration: {
-        start: 30,
-        end: 60,
-      },
-      amountDue: '$3,454.00',
-      shippingAddressId,
-      deliveryMethodId: order?.deliveryMethodId || 0,
-      items,
-      timePeriod: {
-        start: 420,
-        end: 1200,
-      },
-    } satisfies DeliveryFormValues
-  }, [order, orderAddresses])
+  const initValues = useMemo(
+    () =>
+      prepareDeliveryFormData({
+        order,
+        addresses: orderAddresses,
+        defaultValues: {
+          coiNotes: 'test',
+          coiRequired: false,
+          amountDue: '$0.00',
+          deliveryMethodId: 7,
+          estimatedDuration: {
+            start: 120,
+            end: 240,
+          },
+          // title: 'test',
+          notes: 'please call 30 min in advance',
+        },
+        deliveryMethods,
+      }),
+    [order, orderAddresses, deliveryMethods]
+  )
 
   if (!order) {
     return <div>Loading...</div>
   }
   console.log('order', order)
   return (
-    <Box
-      p={2}
-      // maxWidth={1100}
-    >
-      {/* <Button onClick={toggleLayout}>Layout = {layout}</Button>
-      <Button onClick={toggleImage}>Image = {String(image)}</Button>
-      <Button onClick={cycleSize}>{size}</Button> */}
+    <Box p={2}>
       <DeliveryForm
-        order={order}
-        initValues={initValues}
-        addresses={orderAddresses}
-        deliveryMethods={deliveryMethods}
+        comments={initValues.comments}
+        products={initValues.products}
+        initValues={initValues.initValues}
+        addresses={initValues.addresses}
+        deliveryMethods={initValues.deliveryMethods}
         onNewAddress={onNewAddress}
+        onSubmit={(data) => {
+          console.log('submitting data', data)
+          createDelivery(data)
+            .map((newDelivery) => {
+              console.log('newDelivery', newDelivery)
+              snack.success('Delivery created')
+              return newDelivery
+            })
+            .mapErr((err) => {
+              snack.error(String(err))
+              return err
+            })
+        }}
       />
     </Box>
   )
