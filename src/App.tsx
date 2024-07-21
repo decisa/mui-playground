ebda7b824ea84690ab01f4f4a6980c578f12dbd3
Artem Telesh <decarea@yahoo.com>
2024-07-20 23:01:07 -0400

    added fonts to fix display issues on mac

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import './index.css'
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import { CssBaseline, Box } from '@mui/material'
 import { ReactNode } from 'react'
 import { Outlet } from 'react-router'
