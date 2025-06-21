7acc9ec655281afb0ae1f393eb868155d482fd02
Artem Telesh <decarea@yahoo.com>
2025-06-21 10:45:38 -0400

    added action to SET_CROSSHAIR_MARKER

--- src/Components/Maps/mapsReducer.ts
+++ src/Components/Maps/mapsReducer.ts
@@ -1,7 +1,13 @@
-import { ViewState } from 'react-map-gl'
+// import { ViewState } from 'react-map-gl'
+// import { act } from 'react-dom/test-utils'
 import { MapState } from './useMap'
+// import { Marker } from './MapMarker'
 
-export const mapActions = ['SET_VIEW', 'SET_MARKERS'] as const
+export const mapActions = [
+  'SET_VIEW',
+  'SET_MARKERS',
+  'SET_CROSSHAIR_MARKER',
+] as const
 export type MapActions = (typeof mapActions)[number]
 
 type Action<T extends MapActions, P> = {
@@ -13,12 +19,16 @@ type SetViewAction = Action<'SET_VIEW', Partial<MapState['viewState']>>
 
 type SetMarkersAction = Action<'SET_MARKERS', MapState['markers']>
 
-export type MapReducerActions = SetViewAction | SetMarkersAction
+// set 1 marker and zoom to it
+type SetMarker = Action<'SET_CROSSHAIR_MARKER', MapState['markers'][0] | null>
+
+export type MapReducerActions = SetViewAction | SetMarkersAction | SetMarker
 
 export function mapReducer(
   state: MapState,
   action: MapReducerActions
 ): MapState {
+  let markers: MapState['markers']
   switch (action.type) {
     case 'SET_VIEW':
       return {
@@ -32,6 +42,19 @@ export function mapReducer(
       return {
         ...state,
         markers: action.payload,
+      }
+    case 'SET_CROSSHAIR_MARKER':
+      markers = action.payload ? [{ ...action.payload, type: 'crosshair' }] : []
+      return {
+        ...state,
+        markers,
+        viewState: {
+          ...state.viewState,
+          latitude: action.payload?.latitude || 40.1110498,
+          longitude: action.payload?.longitude || -75.0036599,
+          zoom: action.payload ? 17 : 6,
+          pitch: 0,
+        },
       }
     default:
       return state
