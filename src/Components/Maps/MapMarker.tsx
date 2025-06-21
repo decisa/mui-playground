161d3d0e12914b18d8f3dc6380fb1ce327e0e539
Artem Telesh <decarea@yahoo.com>
2025-06-21 10:21:01 -0400

    added marker types and option to switch to crosshair instead of circle

--- src/Components/Maps/MapMarker.tsx
+++ src/Components/Maps/MapMarker.tsx
@@ -1,27 +1,48 @@
 import { memo } from 'react'
-import type { Marker as MapboxMarker } from 'mapbox-gl'
-import { Marker, MarkerProps } from 'react-map-gl'
+import type { Marker as MapboxMarkerType } from 'mapbox-gl'
+import { useTheme } from '@mui/material/styles'
+import {
+  Marker as ReactMapMarker,
+  MarkerProps as ReactMapMarkerProps,
+} from 'react-map-gl'
 import Pin, { MarkerSize } from './Pin'
+
+export type MarkerType = 'crosshair' | 'circle'
+
+export type Marker = {
+  label: string
+  number: number
+  color?: string
+  latitude: number
+  longitude: number
+  type?: MarkerType
+  onDragEnd?: ReactMapMarkerProps['onDragEnd']
+}
+
+// export type MapMarkerPropsOld = {
+//   size: MarkerSize
+//   label: string
+//   number: number
+// } & ReactMapMarkerProps &
+//   React.RefAttributes<MapboxMarkerType>
 
 export type MapMarkerProps = {
   size: MarkerSize
-  label: string
-  number: number
-} & MarkerProps &
-  React.RefAttributes<MapboxMarker>
+  marker: Marker
+} & Omit<
+  ReactMapMarkerProps,
+  'latitude' | 'longitude' | 'color' | 'onDragEnd'
+> &
+  React.RefAttributes<MapboxMarkerType>
 
-function MapMarker({
-  size = 'large',
-  label,
-  latitude,
-  longitude,
-  number,
-  color,
-}: MapMarkerProps) {
+function MapMarker({ size = 'large', marker }: MapMarkerProps) {
+  const theme = useTheme()
+  console.log(`render MapMarker ${marker.label}`, marker)
+  const { label, latitude, longitude, number, color, onDragEnd } = marker
   // console.log(`render MapMarker ${label}`)
   console.log(`render MapMarker`)
   return (
-    <Marker
+    <ReactMapMarker
       latitude={latitude !== undefined ? latitude : 40.11105997742595}
       longitude={longitude !== undefined ? longitude : -75.00333499557553}
       anchor="bottom"
@@ -29,9 +50,16 @@ function MapMarker({
       onClick={(e) => {
         console.log('marker clicked', e)
       }}
+      onDragEnd={onDragEnd}
     >
-      <Pin size={size} label={label} number={number} color={color} />
-    </Marker>
+      <Pin
+        size={size}
+        label={label}
+        number={number}
+        color={color || theme.palette.primary.dark}
+        type={marker.type}
+      />
+    </ReactMapMarker>
   )
 }
 
